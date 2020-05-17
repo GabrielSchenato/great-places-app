8f4d0eec749f9444021b71c414ef23e64c2e0d67
Gabriel Schenato <gabrielschenato152@hotmail.com>
2020-05-17 18:45:04 -0300

    Features

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+
+import PlacesNavigator from './navigation/PlacesNavigator';
+import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
+
+init().then(() => {
+  console.log('Initialized database.');
+}).catch(error => {
+  console.log('Initializing database failed.');
+  console.log(error);
+});
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <PlacesNavigator />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
